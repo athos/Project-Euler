8c2491674e9eddcfc7d04d7ab1f1c1108de73c49
OHTA Shogo <athos0220@gmail.com>
2010-11-22 23:49:19 +0900

    solved Problem 2

---
+++ problem002.clj
@@ -0,0 +1,21 @@
+;; Problem 2
+
+;; Each new term in the Fibonacci sequence is generated
+;; by adding the previous two terms. By starting with
+;; 1 and 2, the first 10 terms will be:
+
+;; 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
+
+;; Find the sum of all the even-valued terms in the sequence
+;; which do not exceed four million.
+
+(ns problem002)
+
+(defn fib
+  ([] (fib 0 1))
+  ([a b]
+   (lazy-seq
+     (cons b (fib b (+ a b))))))
+
+(defn solve []
+  (reduce + (take-while #(<= % 4000000) (filter even? (fib)))))
