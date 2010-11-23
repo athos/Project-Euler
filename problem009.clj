fba63d749cc4724fdbfa1761158ef8e9f6f227bc
OHTA Shogo <athos0220@gmail.com>
2010-11-23 14:42:42 +0900

    solved Problem 9

--- problem009.clj
+++ problem009.clj
@@ -13,4 +13,7 @@
 (ns problem009)
 
 (defn solve []
-  nil)
+  (first (for [a (range 1 1000), b (range (inc a) 1000)
+	       :let [c (- 1000 a b)]
+	       :when (= (+ (* a a) (* b b)) (* c c))]
+	   (* a b c))))
