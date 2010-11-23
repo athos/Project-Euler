f61810e84d1a7b390e08a02b3f2dd4bc48819ceb
OHTA Shogo <athos0220@gmail.com>
2010-11-23 16:03:31 +0900

    make primes-under more efficient

--- utils.clj
+++ utils.clj
@@ -7,18 +7,22 @@
   (int (Math/ceil n)))
 
 (defn primes-under [n]
-  (letfn [(cleanup [t]
-	    (loop [i 2, t t]
-	      (cond (>= i n) t
-		    (nth t i) (recur (inc i) (assoc! t i i))
-		    :else (recur (inc i) t))))]
-    (let [max (ceiling (Math/sqrt n))]
-      (loop [i (int 2), t (transient (vec (repeat n true)))]
-	(cond (> i max) (filter number? (persistent! (cleanup t)))
-	      (not (nth t i)) (recur (inc i) t)
-	      :else (letfn [(iter [t]
-			      (loop [j (int (* 2 i)), t t]
-				(if (< j n)
-				  (recur (+ j i) (assoc! t j false))
-				  t)))]
-		      (recur (inc i) (iter t))))))))
+  (let [n* (ceiling (/ n 2))]
+    (letfn [(val-at [i]
+	      (+ (* 2 i) 3))
+	    (cleanup [t]
+	      (loop [i 0, t t]
+		(cond (>= i n*) t
+		      (nth t i) (recur (inc i) (assoc! t i (val-at i)))
+		      :else (recur (inc i) t))))]
+      (let [max (ceiling (Math/sqrt n*))]
+	(loop [i (int 0), t (transient (vec (repeat n* true)))]
+	  (cond (> i max) (cons 2 (filter number? (persistent! (cleanup t))))
+		(not (nth t i)) (recur (inc i) t)
+		:else (letfn [(iter [t]
+				(loop [j (int (+ i (val-at i))), t t]
+				  (if (< j n*)
+				    (recur (+ j (int (val-at i)))
+					   (assoc! t j false))
+				    t)))]
+			(recur (inc i) (iter t)))))))))
