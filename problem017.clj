86250f8d91a72dee7f86ae1b048026c9cc66f11f
OHTA Shogo <athos0220@gmail.com>
2010-11-24 11:34:14 +0900

    solved Problem 17

--- problem017.clj
+++ problem017.clj
@@ -13,7 +13,33 @@
 ;; The use of "and" when writing out numbers is in compliance
 ;; with British usage.
 
-(ns problem017)
+(ns problem017
+  (:use [clojure.contrib.string :only (join)]))
+
+(def number->word
+  (sorted-map
+    1 "one", 2 "two", 3 "three", 4 "four", 5 "five",
+    6 "six", 7 "seven", 8 "eight", 9 "nine", 10 "ten",
+    11 "eleven", 12 "twelve", 13 "thirteen", 14 "fourteen", 15 "fifteen",
+    16 "sixteen", 17 "seventeen", 18 "eighteen", 19 "nineteen", 20 "twenty",
+    30 "thirty", 40 "forty", 50 "fifty", 60 "sixty", 70 "seventy",
+    80 "eighty", 90 "ninety", 100 "hundred", 1000 "thousand"))
+
+(defn number->words [n]
+  (loop [n n, [[m w] & ws] (rseq number->word), ret []]
+    (cond (= n 0) (join " " ret)
+	  (>= n m)
+	  (if (or (= m 1000) (= m 100))
+	    (recur (mod n m) ws
+		   (conj ret
+			 (number->word (quot n m))
+			 (number->word m)
+			 (if (and (= m 100) (not= (mod n m) 0))
+			   "and"
+			   "")))
+	    (recur (- n m) ws (conj ret (number->word m))))
+	  :else (recur n ws ret))))
 
 (defn solve []
-  nil)
+  (reduce + (map #(count (remove #{\space \-} (number->words %)))
+		 (range 1 1001))))
