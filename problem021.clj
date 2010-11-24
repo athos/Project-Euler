a360c5a97dfe4f6950a2efd2f566722f2cc1edd7
OHTA Shogo <athos0220@gmail.com>
2010-11-24 18:14:55 +0900

    solved Problem 21

---
+++ problem021.clj
@@ -0,0 +1,41 @@
+;; Problem 21
+
+;; Let d(n) be defined as the sum of proper divisors of n
+;; (numbers less than n which divide evenly into n).
+;; If d(a) = b and d(b) = a, where a ≠ b, then a and b are
+;; an amicable pair and each of a and b are called amicable numbers.
+
+;; For example, the proper divisors of 220 are 1, 2, 4, 5, 10, 11,
+;; 20, 22, 44, 55 and 110; therefore d(220) = 284. The proper
+;; divisors of 284 are 1, 2, 4, 71 and 142; so d(284) = 220.
+
+;; Evaluate the sum of all the amicable numbers under 10000.
+
+(ns problem021
+  (:use [utils :only (ceiling |)]))
+
+(defn divisors-of [n]
+  (let [n* (int (ceiling (Math/sqrt n)))]
+    (loop [d (int 1), ds []]
+      (cond (> d n*) ds
+	    (| n d)
+	    (if (= d n*)
+	      (recur (inc d) (conj ds d))
+	      (recur (inc d) (conj ds (/ n d) d)))
+	    :else (recur (inc d) ds)))))
+
+(defn sum-of-proper-divisors [n]
+  (reduce + (rest (divisors-of n))))
+
+(defn amicable-numbers-under [max]
+  (loop [i 1, as #{}]
+    (cond (>= i max) as
+	  (as i) (recur (inc i) as)
+	  :else (let [n (sum-of-proper-divisors i)
+		      m (sum-of-proper-divisors n)]
+		  (if (and (= m i) (not= i n) (< n max))
+		    (recur (inc i) (conj as i n))
+		    (recur (inc i) as))))))
+
+(defn solve []
+  (reduce + (amicable-numbers-under 10000)))
