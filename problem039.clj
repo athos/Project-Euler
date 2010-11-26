355f9f89f596342ebbbc9d9512307ef8eddd32a3
OHTA Shogo <athos0220@gmail.com>
2010-11-26 15:41:23 +0900

    solved Problem 39

--- problem039.clj
+++ problem039.clj
@@ -7,7 +7,30 @@
 
 ;; For which value of p ≤ 1000, is the number of solutions maximised?
 
-(ns problem039)
+(ns problem039
+  (:use [utils :only (ceiling)]))
+
+(defn solutions-for [p]
+  (let [m (ceiling (/ p 3))]
+    (for [c (range m (- p 3))
+	  b (range m c)
+	  :let [a (- p c b)]
+	  :when (and (< 0 a b c)
+		     (= (+ (* a a) (* b b)) (* c c)))]
+      [a b c])))
+
+(defn find-max-between [start end]
+  (reduce (fn [[p c :as x] [p* c* :as x*]]
+	    (if (> c* c) x* x))
+	  (map (fn [p]
+		 [p (count (solutions-for p))])
+	       (range start end))))
 
 (defn solve []
-  nil)
+  (let [x (future (find-max-between 1 800))
+	y (future (find-max-between 800 1001))
+	[p0 c0] @x
+	[p1 c1] @y]
+    (if (>= c0 c1)
+      p0
+      p1)))
